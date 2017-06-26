1142d42e8a03efebfd472a8976870aa09d4d7e05
unknown <eze.div@gmail.com>
2017-06-26 12:12:03 -0300

    12.11

--- 1-EntradaSalida/jsEntradaSalida-2.js
+++ 1-EntradaSalida/jsEntradaSalida-2.js
@@ -1,7 +1,9 @@
-/*Debemos lograr tomar un nombre con 'prompt' 
+/*Debemos lograr tomar un nombre con 'prompt,alert,confirm' 
 y luego mostrarlo por 'alert' al presionar el botón  'MOSTRAR'*/
 function Mostar()
 {
-	
+	var numero;
+    numero = prompt ("ingrese numer"); 
+    alert (numero);
 }
 
